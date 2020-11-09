ee8a5be1e301c97449453f0b0c27a52386b2487f
Ice Queen <53878781+RomanGoddess@users.noreply.github.com>
2020-11-09 22:50:49 +0100

    Update ffmpeg.py

--- bot/helper_funcs/ffmpeg.py
+++ bot/helper_funcs/ffmpeg.py
@@ -122,7 +122,7 @@ async def convert_video(video_file, output_directory, total_time, bot, message, 
         if difference > 0:
           ETA = TimeFormatter(difference*1000)
         percentage = math.floor(elapsed_time * 100 / total_time)
-        progress_str = "<b>𝗣𝗿𝗼𝗴𝗿𝗲𝘀𝘀:</b> {0}%\n[{1}{2}]".format(
+        progress_str = "<b>𝗣𝗿𝗼𝗴𝗿𝗲𝘀𝘀:</b> {0}%\n《{1}{2}》".format(
             round(percentage, 2),
             ''.join([FINISHED_PROGRESS_STR for i in range(math.floor(percentage / 10))]),
             ''.join([UN_FINISHED_PROGRESS_STR for i in range(10 - math.floor(percentage / 10))])
