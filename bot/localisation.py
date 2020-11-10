582309a9b5a1a886696952633ef2461a03ea1145
Ice Queen <53878781+RomanGoddess@users.noreply.github.com>
2020-11-10 08:11:14 +0100

    Update localisation.py

--- bot/localisation.py
+++ bot/localisation.py
@@ -18,7 +18,7 @@ from bot.get_cfg import get_config
 
 
 class Localisation:
-    START_TEXT = "<b>Hello, My Name Is 𝗖𝗢𝗠𝗣𝗥𝗘𝗦𝗦 𝗩𝗜𝗗𝗘𝗢 𝗕𝗢𝗧 🥳.\n\nI'm A <u>𝗩𝗜𝗗𝗘𝗢 𝗖𝗢𝗠𝗢𝗥𝗘𝗦𝗦𝗢𝗥 𝗕𝗢𝗧</u>\n\nSend Me Any 𝗧𝗲𝗹𝗲𝗴𝗿𝗮𝗺 𝗩𝗶𝗱𝗲𝗼 Be It 𝗗𝗼𝗰𝘂𝗺𝗲𝗻𝘁/𝗦𝘁𝗿𝗲𝗮𝗺𝗮𝗯𝗹𝗲 & I'll 𝗖𝗼𝗺𝗽𝗿𝗲𝘀𝘀 It & Resend You A Smaller Packed Size, See /Help For More Information\n\n❌ 𝗬𝗢𝗨 𝗖𝗔𝗡 𝗢𝗡𝗟𝗬 𝗣𝗘𝗥𝗙𝗢𝗥𝗠 <u>𝗢𝗡𝗘 𝗢𝗣𝗘𝗥𝗔𝗧𝗜𝗢𝗡</u> 𝗧𝗢 𝗔𝗩𝗢𝗜𝗗 𝗕𝗢𝗧 𝗢𝗩𝗘𝗥𝗟𝗢𝗔𝗗.\n\n𝗦𝘂𝗯𝘀𝗰𝗿𝗶𝗯𝗲 𝗧𝗼 @FlixBots 𝗙𝗼𝗿 𝗠𝗼𝗿𝗲 𝗘𝘅𝗰𝗶𝘁𝗶𝗻𝗴 𝗕𝗼𝘁𝘀</b>"
+    START_TEXT = "<b>Hello, My Name Is 𝗖𝗢𝗠𝗣𝗥𝗘𝗦𝗦 𝗩𝗜𝗗𝗘𝗢 𝗕𝗢𝗧 🥳.\n\nI'm A <u>𝗩𝗜𝗗𝗘𝗢 𝗖𝗢𝗠𝗢𝗥𝗘𝗦𝗦𝗢𝗥 𝗕𝗢𝗧</u>\n\nSend Me Any 𝗧𝗲𝗹𝗲𝗴𝗿𝗮𝗺 𝗩𝗶𝗱𝗲𝗼 Be It 𝗗𝗼𝗰𝘂𝗺𝗲𝗻𝘁/𝗦𝘁𝗿𝗲𝗮𝗺𝗮𝗯𝗹𝗲 & I'll 𝗖𝗼𝗺𝗽𝗿𝗲𝘀𝘀 It & Resend You A Smaller Packed Size, See /help For More Information\n\n❌ 𝗬𝗢𝗨 𝗖𝗔𝗡 𝗢𝗡𝗟𝗬 𝗣𝗘𝗥𝗙𝗢𝗥𝗠 <u>𝗢𝗡𝗘 𝗢𝗣𝗘𝗥𝗔𝗧𝗜𝗢𝗡</u> 𝗧𝗢 𝗔𝗩𝗢𝗜𝗗 𝗕𝗢𝗧 𝗢𝗩𝗘𝗥𝗟𝗢𝗔𝗗.\n\n𝗦𝘂𝗯𝘀𝗰𝗿𝗶𝗯𝗲 𝗧𝗼 @FlixBots 𝗙𝗼𝗿 𝗠𝗼𝗿𝗲 𝗘𝘅𝗰𝗶𝘁𝗶𝗻𝗴 𝗕𝗼𝘁𝘀</b>"
     ABS_TEXT = "Please Don't Be Selfish."
     
     FORMAT_SELECTION = "Select The Desired Format: <a href='{}'>file size might be approximate</a> \nIf You Want To Set Custom Thumbnail, Send A Photo Before Or Quickly After Tapping On Any Of The Below Buttons.\nYou Can Use /deletethumbnail To Delete The Auto-Generated Thumbnail."
@@ -34,7 +34,7 @@ class Localisation:
     
     RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 𝟮𝗚𝗕 due to Telegram API limitations."
     
-    COMPRESS_SUCCESS = "<b>📁 Downloaded In {}</b>\n\n<b>📀 Compressed In {}</b>\n\n<b>🌐 Uploadedn In {}</b>\n\n𝗦𝘂𝗯𝘀𝗰𝗿𝗶𝗯𝗲 𝗧𝗼 @FlixBots 𝗙𝗼𝗿 𝗠𝗼𝗿𝗲 𝗘𝘅𝗰𝗶𝘁𝗶𝗻𝗴 𝗕𝗼𝘁𝘀"
+    COMPRESS_SUCCESS = "<b>📁 𝗗𝗼𝘄𝗻𝗹𝗼𝗮𝗱𝗲𝗱 𝗜𝗻 {}</b>\n\n<b>📀 𝗖𝗼𝗺𝗽𝗿𝗲𝘀𝘀𝗲𝗱 𝗜𝗻 {}</b>\n\n<b>🌐 𝗨𝗽𝗹𝗼𝗮𝗱𝗲𝗱 𝗜𝗻 {}</b>\n\n𝗦𝘂𝗯𝘀𝗰𝗿𝗶𝗯𝗲 𝗧𝗼 @FlixBots 𝗙𝗼𝗿 𝗠𝗼𝗿𝗲 𝗘𝘅𝗰𝗶𝘁𝗶𝗻𝗴 𝗕𝗼𝘁𝘀"
 
     COMPRESS_PROGRESS = "<b>𝗘𝗧𝗔 :</b> {}\n<b>𝗣𝗿𝗼𝗴𝗿𝗲𝘀𝘀 :</b> {}%"
 
@@ -44,7 +44,7 @@ class Localisation:
     
     FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "Media Cleared Succesfully. ❌"
     
-    SAVED_RECVD_DOC_FILE = "Downloaded Successfully. 📩"
+    SAVED_RECVD_DOC_FILE = "𝗠𝗲𝗱𝗶𝗮 𝗗𝗼𝘄𝗻𝗹𝗼𝗮𝗱𝗲𝗱 𝗦𝘂𝗰𝗰𝗲𝘀𝘀𝗳𝘂𝗹𝗹𝘆 📩."
     
     CUSTOM_CAPTION_UL_FILE = " "
     
@@ -54,7 +54,7 @@ class Localisation:
     
     USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> added to {} till {}."
     
-    FF_MPEG_RO_BOT_STOR_AGE_ALREADY_EXISTS = "<b>One Process Ongoing Right Now ⚠️.</b>\n\n<b>Or The Media Already Exists.</b>\n\n<b>Please send /cancel to delete existing media.</b>"
+    FF_MPEG_RO_BOT_STOR_AGE_ALREADY_EXISTS = "<b>𝗢𝗻𝗲 𝗣𝗿𝗼𝗰𝗲𝘀𝘀 𝗜𝘀 𝗢𝗻𝗴𝗼𝗶𝗻𝗴 𝗥𝗶𝗴𝗵𝘁 𝗡𝗼𝘄 ⚠️.\n\n𝗣𝗹𝗲𝗮𝘀𝗲 𝗦𝗲𝗻𝗱 /cancel 𝗧𝗼 𝗗𝗲𝗹𝗲𝘁𝗲 𝗘𝘅𝗶𝘀𝘁𝗶𝗻𝗴 𝗠𝗲𝗱𝗶𝗮.</b>"
     
     HELP_MESSAGE = get_config(
         "STRINGS_HELP_MESSAGE",
